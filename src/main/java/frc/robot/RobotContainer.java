a7966cdcd44114c1a8ab7241197c36e79706be87
Chris J <greatnessof42@gmail.com>
2026-03-09 20:46:41 -0400

    Add auto chooser to dashboard

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,7 @@ import com.pathplanner.lib.auto.AutoBuilder;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -47,6 +48,7 @@ public class RobotContainer {
         autoBuilder = AutoBuilder.buildAutoChooserWithOptionsModifier(
             stream -> stream.filter(s -> s.getName().startsWith("Woodhaven"))
         );
+        SmartDashboard.putData(autoBuilder);
     }
 
     private void configureBindings() {
