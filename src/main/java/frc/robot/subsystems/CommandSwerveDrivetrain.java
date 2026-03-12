0a6f1d114133adf16827da00690f00c2f15822bb
kransaw <kameronthescreechyflute@gmail.com>
2026-03-12 19:07:00 -0400

    Added subsystems and field2d to Elastic / Smart Dashboard

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.*;
+import static edu.wpi.first.units.Units.Second;
+import static edu.wpi.first.units.Units.Volts;
 
 import java.util.Optional;
 import java.util.function.Supplier;
@@ -26,6 +27,8 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -55,6 +58,9 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
     private final SwerveRequest.SysIdSwerveTranslation m_translationCharacterization = new SwerveRequest.SysIdSwerveTranslation();
     private final SwerveRequest.SysIdSwerveSteerGains m_steerCharacterization = new SwerveRequest.SysIdSwerveSteerGains();
     private final SwerveRequest.SysIdSwerveRotation m_rotationCharacterization = new SwerveRequest.SysIdSwerveRotation();
+
+    // Field 2d
+    private final Field2d matchField = new Field2d();
 
     /*
      * SysId routine for characterizing translation. This is used to find PID gains
@@ -137,6 +143,8 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
             startSimThread();
         }
         configurePathPlanner();
+        // Do this in either robot or subsystem init
+        SmartDashboard.putData("Field", matchField);
     }
 
     /**
@@ -163,6 +171,8 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
             startSimThread();
         }
         configurePathPlanner();
+        // Do this in either robot or subsystem init
+        SmartDashboard.putData("Field", matchField);
     }
 
     /**
@@ -204,6 +214,8 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
             startSimThread();
         }
         configurePathPlanner();
+        // Do this in either robot or subsystem init
+        SmartDashboard.putData("Field", matchField);
     }
 
     public void configurePathPlanner(){
@@ -292,6 +304,9 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
                 m_hasAppliedOperatorPerspective = true;
             });
         }
+
+        // Do this in either robot periodic or subsystem periodic
+        matchField.setRobotPose(this.getPose());
     }
 
     private void startSimThread() {
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -17,6 +17,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -37,6 +38,8 @@ public class ShooterSubsystem extends SubsystemBase {
         configureMotor(rightShooterMotor, InvertedValue.Clockwise_Positive);
         configureMotor(leftShooterMotor, InvertedValue.CounterClockwise_Positive); // inverted
         configureMotor(accelerator, InvertedValue.CounterClockwise_Positive);
+
+        SmartDashboard.putData(this); 
     }
 
     private void configureMotor(TalonFX motor, InvertedValue invertDirection) {
