d76b8feb1214331c64227ac51e2befa80244a451
Chris J <greatnessof42@gmail.com>
2026-03-30 15:57:51 -0400

    Push missed files, update libraries

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -23,6 +23,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
@@ -250,7 +251,10 @@ public class RobotContainer {
     }
 
     public Command startAutoAim(){
-        return leftTurretSubsystem.enableAutoAim().alongWith(rightTurretSubsystem.enableAutoAim());
+        return Commands.run(() -> {
+                leftTurretSubsystem.isAutoAim = true;
+                rightTurretSubsystem.isAutoAim = true;
+        });
     }
 
     public Command getAutonomousCommand() {
--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.Second;
 import static edu.wpi.first.units.Units.Volts;
 
@@ -21,8 +22,12 @@ import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Velocity;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
@@ -259,6 +264,12 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
      */
     public Command applyRequest(Supplier<SwerveRequest> request) {
         return run(() -> this.setControl(request.get()));
+    }
+
+    public LinearVelocity getForwardVelocity(){
+        SwerveModuleState[] moduleStates = this.getState().ModuleStates;
+        ChassisSpeeds speeds = this.getKinematics().toChassisSpeeds(moduleStates);
+        return MetersPerSecond.of(speeds.vxMetersPerSecond);
     }
 
     /**
